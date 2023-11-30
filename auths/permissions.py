de62e0d17dadb2f9368644e0b256c2434aa4120d
Nihal <nihalroshan55@gmail.com>
2023-11-30 19:31:12 +0530

    added comments in permissions.py

--- auths/permissions.py
+++ auths/permissions.py
@@ -1,11 +1,15 @@
 from rest_framework import permissions
 
+
+# Custom permissions for authorisation
+
 class IsTeacherOrAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         user = request.user
+        # returning True if the requested user.user_type is teacher or the user is superuser
         return  (user.user_type == 'teacher' or user.is_superuser)
     
 class IsStudent(permissions.BasePermission):
     def has_permission(self, request, view):
-        # Check if the user has the 'student' group
+        # Check if the user has the 'student' or the user IsTeacherOrAdmin 
         return request.user.groups.filter(name='student').exists() or IsTeacherOrAdmin()
